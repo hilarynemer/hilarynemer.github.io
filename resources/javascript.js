845f6e92da75ac0dc752b26db158ea3cbe48e6ad
Hilary Nemer <hilary@tradecrafted.com>
2014-01-10 17:56:54 -0800

    changed styling

--- resources/javascript.js
+++ resources/javascript.js
@@ -1,1 +1,14 @@
-// Your Javascript will go here!
+
+  // $document.onclick = replaceName;
+
+$(document).ready(function() {
+	$("#copyright").click(function(){ 
+	  $(this).css('color','purple'); 
+	});
+});
+
+
+// function replaceName() {
+//     var myNewName = window.prompt('Pick a new name!');
+//     document.getElementById('username').innerHTML = myNewName;
+//   }
